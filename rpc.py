1d03fd8e7f9c24dc9c182a93d4ff9147b1dc5923
NeynQ <114482884+NeynQ@users.noreply.github.com>
2023-03-16 19:39:20 +0300

    Update rpc.py

--- rpc.py
+++ rpc.py
@@ -11,18 +11,21 @@ def getTrack():
     last_queue = client.queue(queues[0].id)
     last_track_id = last_queue.get_current_track()
     last_track = last_track_id.fetch_track()
+    tid = last_track_id.track_id
+    url = f"https://music.yandex.ru/track/{tid}"
     artists = ', '.join(last_track.artists_name())
     title = last_track.title
-    return artists, title
+    return artists, title, url
 
 client_id = '1079901231817965598'
 RPC = Presence(client_id)
-RPC.connect()
+RPC.connect() # mq
 
 while True:
     try:
         track = getTrack()
         RPC.update(
+            buttons=[{"label": "Слушать", "url": track[2]}],
             state="▶ " + track[1],
             details="" + track[0],
             large_image="ym_avatar",

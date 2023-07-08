036a887cf911a46eed5b128d6e7fc68c7bbcaf5b
NeynQ <114482884+NeynQ@users.noreply.github.com>
2023-07-08 15:27:29 +0300

    Update rpc.py
    BETA BLYAT!!!

--- rpc.py
+++ rpc.py
@@ -1,12 +1,13 @@
 from pypresence import Presence
 from yandex_music import Client
 from yandex_music.exceptions import UnauthorizedError
+from json import load, dump
 import time
 
-from config import access_token
+import config as cfg
 
 def getTrack():
-    client = Client(access_token).init()
+    client = Client(cfg.access_token).init()
     queues = client.queues_list()
     last_queue = client.queue(queues[0].id)
     last_track_id = last_queue.get_current_track()
@@ -15,11 +16,25 @@ def getTrack():
     url = f"https://music.yandex.ru/track/{tid}"
     artists = ', '.join(last_track.artists_name())
     title = last_track.title
-    return artists, title, url
+    photo = last_track.getCoverUrl()
+    return artists, title, url, photo
 
-client_id = '1079901231817965598'
+client_id = '1114742401878204466'
 RPC = Presence(client_id)
 RPC.connect() # mq
+
+def getTimer() -> int:
+    title = getTrack()[1]
+    with open("config.json", "r") as f:
+            data = load(f)
+    track = data["track"]
+    if title == track:
+        return int(data["time"])
+    else:
+        with open("config.json", "w") as f:
+            data["track"] = title
+            data["time"] = time.time()
+            data = dump(data, f)
 
 while True:
     try:
@@ -28,17 +43,21 @@ while True:
             buttons=[{"label": "Слушать", "url": track[2]}],
             state="▶ " + track[1],
             details="" + track[0],
-            large_image="ym_avatar",
-            large_text="Yandex Music"
+            large_image=track[3],
+            large_text="Yandex Music",
+            small_image="ym_avatar",
+            small_text="#NoWar",
+            start=getTimer()
         )
     except (UnauthorizedError, UnicodeEncodeError):
         print("Неверный access_token!")
         print("Введите верный токен в файле config.py!")
         break
-    except:
+    except Exception as e:
+        print(e)
+        print("Отправьте скриншот ошибки DS: neynq или TG: @neynq")
         RPC.update(
             state="Сейчас ничего не играет!",
             large_image="ym_avatar",
             large_text="Yandex Music"
         )
-    time.sleep(15)
